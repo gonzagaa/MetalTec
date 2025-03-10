79d8284fff024b72709ad0eba754185f3a52e6ae
gonzagaa <iigust4vo@gmail.com>
2025-03-10 10:54:03 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,131 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+    showNavOnScroll()
+}
+
+function showNavOnScroll() {
+    if(scrollY > 0) {
+        document.querySelector("#navigation").classList.add("scroll")
+    } else {
+        document.querySelector("#navigation").classList.remove("scroll")
+    }
+}
+
+function showButtonWhatsappOnScroll() {
+    if(scrollY > 200) {
+        document.querySelector("#buttonWhatsapp").classList.add("show")
+    } else {
+        document.querySelector("#buttonWhatsapp").classList.remove("show")
+    }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+
+const larguraDaTela = window.innerWidth
+
+
+if (larguraDaTela < 800) {
+  var swiper4 = new Swiper(".mySwiper4", {
+    cssMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+  });
+} else {
+  var swiper4 = new Swiper(".mySwiper4", {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      loop: true,
+      grabCursor: true,
+      navigation: {
+        nextEl: ".swiper-button-prev",
+        prevEl: ".swiper-button-next",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+}
+
+if (larguraDaTela < 800) {
+  var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 1,
+    cssMode: true,
+    spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+      loop: true,
+  });
+} else {
+  var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 3,
+    cssMode: true,
+    spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+      loop: true,
+  });
+}
+
+if (larguraDaTela < 800) {
+  var swiper5 = new Swiper(".mySwiper5", {
+    slidesPerView: 2,
+    cssMode: true,
+    spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+      loop: true,
+  });
+} else {
+  var swiper5 = new Swiper(".mySwiper5", {
+    slidesPerView: 5,
+    cssMode: true,
+    spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+      loop: true,
+  });
+}
